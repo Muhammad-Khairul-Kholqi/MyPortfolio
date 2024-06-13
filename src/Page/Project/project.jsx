1c2e84d272dc47834f9626497ff57338747c0f93
Muhammad-Khairul-Kholqi <khairulkhlqi31@gmail.com>
2024-06-13 12:50:28 +0700

    update card project

--- src/Page/Project/project.jsx
+++ src/Page/Project/project.jsx
@@ -1,64 +1,34 @@
-// import React, { useEffect, useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import CardProject from "./cardProject"
-// import ProjectsData from "./projectsData.jsx";
-
-// const Project = () => {
-//   const [projects, setProjects] = useState([]);
-
-//   useEffect(() => {
-//     setProjects(ProjectsData);
-//   }, []);
-
-//   return (
-//     <>
-//         <div className="pb-[20px]">
-//             <p className="text-[24px]">Projects</p>
-//             <p className="text-[16px] text-[#525252]">Showcasing my passion for technology, design, and problem-solving through code.</p>
-//         </div>
-
-//         <hr className="border-t border-dashed border-gray-500" />
-
-//         <div className="project-list flex flex-wrap justify-start">
-//         {projects.map((project) => (
-//             <CardProject key={project.id} project={project} />
-//         ))}
-//         </div>
-//     </>
-//   );
-// };
-
-// export default Project;
-
-
 import React, { useEffect, useState } from 'react';
 import CardProject from "./cardProject";
+
+import schoolImage from '../../Assets/project/school.png';
+import githubImage from '../../Assets/skills/github.png';
 
 const projectsData = [
   {
     id: 1,
     title: "Project 1",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut suscipit nam esse at? Ipsam, perferendis.",
-    image: "./src/Assets/project/school.png",
+    image: schoolImage,
     technology: ["React", "Node.js"],
     github: "https://github.com/user/project1",
     demo: "https://project1.demo",
     techImages: [
-      "./src/Assets/skills/github.png",
-      "./src/Assets/skills/github.png"
+      githubImage,
+      githubImage
     ]
   },
   {
     id: 2,
     title: "Project 2",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut suscipit nam esse at? Ipsam, perferendis.",
-    image: "./src/Assets/project/school.png",
+    image: schoolImage,
     technology: ["Vue", "Firebase"],
     github: "https://github.com/user/project2",
     demo: "https://project2.demo",
     techImages: [
-      "./src/Assets/skills/github.png",
-      "./src/Assets/skills/github.png"
+      githubImage,
+      githubImage
     ]
   }
 ];
